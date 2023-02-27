3327b1bbb73e69146dd26d54c103da3c23808791
Yunjung Jo <j_yunj@naver.com>
2023-02-27 17:07:39 +0900

    asdfasdfasdf

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-$(function(){
+$(function () {
   $('#fullpage').fullpage({
-		//options here
-		autoScrolling:true,
-		scrollHorizontally: true
-	});
-
+    //options here
+    anchors: ['section1', 'section2', 'section3', 'section4', 'section5'],
+    // autoScrolling:true,
+    // scrollHorizontally: true,
+    navigation: true,
+    sectionsColor: ['#ccc', '#ddd', '#bbb', '#eee', '#fff'],
+    afterLoad: function (anchorLink, index) {
+      console.log('현재 섹션번호는 ' + index)
+      // if (index == 5) {
+      //   alert('마지막입니다')
+      // }
+    },
+    afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) {
+      console.log('현재 슬라이드 번호' + slideIndex)
+    },
+  })
 })
